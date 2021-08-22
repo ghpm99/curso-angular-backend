926e35a5e01260a1b3002db74b3732e46d00d916
Guilherme Henrique <ghpm99@hotmail.com>
2021-08-22 11:17:26 -0300

    Cria projeto vazio

---
+++ src/test/java/com/digitalinnovationone/cursoangularbackend/CursoAngularBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.cursoangularbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoAngularBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
